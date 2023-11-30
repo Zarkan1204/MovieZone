81ea584cf3d61e43c9653b5d8c0df536092c4db4
Artem Gerasimov <artemtrim@mail.ru>
2023-11-30 10:40:25 +0300

    Add collection and table to home

--- MovieZone/Controllers/HomeViewController.swift
+++ MovieZone/Controllers/HomeViewController.swift
@@ -9,9 +9,64 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private let homeTableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
+        setupViews()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        homeTableView.frame = view.bounds
+    }
+    
+    private func setupViews() {
         view.backgroundColor = .systemBackground
+        view.addSubview(homeTableView)
+       
+        homeTableView.delegate = self
+        homeTableView.dataSource = self
+        homeTableView.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension HomeViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension HomeViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
     }
 }
---
+++ MovieZone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  CollectionViewTableViewCell.swift
+//  MovieZone
+//
+//  Created by Мой Macbook on 30.11.2023.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+  static let identifier = "CollectionViewTableViewCell"
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 140, height: 200)
+        layout.scrollDirection = .horizontal
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collection
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+    
+    private func setupView() {
+        contentView.backgroundColor = .yellow
+        addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension CollectionViewTableViewCell: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension CollectionViewTableViewCell: UICollectionViewDelegate {
+    
+}
